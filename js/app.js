a9df5cdf7b9f00edbe7eca05a45e549d9078ea00
Tom Charlet <charlettom11@gmail.com>
2022-03-28 11:59:17 +0200

    Ajout interaction galerie

--- js/app.js
+++ js/app.js
@@ -16,19 +16,30 @@ window.addEventListener("load", () => {
     //     })
     // });
 
-    const btnDrop = document.querySelectorAll(".btn-drop");
-    const dropdown = document.querySelector(".dropdown").offsetHeight;
-    btnDrop.forEach(element => {
-        element.addEventListener('click', () => {
-            let parent = element.closest('.dropdown')
-            if (parent.offsetHeight === dropdown) {
-                parent.style.height = `${parent.scrollHeight}px`;
-            } else {
-                parent.style.height = `${dropdown}px`;
-            }
-        })
-    });
+    // const btnDrop = document.querySelectorAll(".btn-drop");
+    // const dropdown = document.querySelector(".dropdown").offsetHeight;
+    // btnDrop.forEach(element => {
+    //     element.addEventListener('click', () => {
+    //         let parent = element.closest('.dropdown')
+    //         if (parent.offsetHeight === dropdown) {
+    //             parent.style.height = `${parent.scrollHeight}px`;
+    //         } else {
+    //             parent.style.height = `${dropdown}px`;
+    //         }
+    //     })
+    // });
 
     // const vertSlider = document.querySelectorAll(".article-slider");
     // console.log(vertSlider);
+
+    document.querySelectorAll('.galerie-photo-items img').forEach(image => {
+        image.addEventListener('click', () => {
+            document.querySelector('.popup-image').style.display = 'block';
+            document.querySelector('.popup-image img').src = image.getAttribute('src');
+        }
+        )
+    });
+    document.querySelector('.popup-image-close').onclick = () => {
+        document.querySelector('.popup-image').style.display = 'none';
+    }
 });
