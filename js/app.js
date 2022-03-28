87cc768231096fcea9b662577053a9ab393dddc8
Tom Charlet <charlettom11@gmail.com>
2022-03-28 19:38:53 +0200

    Finitions lien et icones

--- js/app.js
+++ js/app.js
@@ -24,48 +24,6 @@ window.addEventListener("load", () => {
         });
     });
 
-
-
-
-
-    // RESULTATS MATCHS
-
-    var match1 = {
-        mois: 'OCTOBRE',
-        teamName1: 'Equipe 1',
-        teamLogo1: '../img/logo-ems.png',
-        score: '1 - 3',
-        teamName2: 'Equipe 2',
-        teamLogo2: '../img/logo-ems.png',
-        desc: 'MATCHS TEST - 12 OCOTBRE 2022 - 8H50 - ELBEUF'
-    };
-    console.log(match1);
-    var match2 = {
-        mois: 'NOVEMBRE',
-        teamName1: 'Equipe 1 autre',
-        teamLogo1: '../img/logo-ems.png',
-        score: ' - ',
-        teamName2: 'Equipe 2 autre',
-        teamLogo2: '../img/logo-ems.png',
-        desc: 'TEST TEST - 2 NOVEMBRE 2023 - 18H20 - ROUEN'
-    };
-
-    var mois = document.querySelector('.carousel-info');
-
-    console.log(mois.textContent);
-    mois = match1.mois;
-    console.log(mois);
-    mois.innerHTML(match1.mois);
-
-    document.querySelector('.btn-next').addEventListener('click', () => {
-        mois.innerHTML("<h3 class='carousel-info'>" + match1.mois + "</h3>");
-        console.log(match1.mois);
-    });
-
-
-
-
-
     // POPUP IMAGES
 
     document.querySelectorAll('.galerie-photo-items img').forEach(image => {
@@ -77,4 +35,38 @@ window.addEventListener("load", () => {
     document.querySelector('.popup-image-close').addEventListener('click', () => {
         document.querySelector('.popup-image').style.display = 'none';
     });
+
+    // RESULTATS MATCHS
+
+    // var match1 = {
+    //     mois: 'OCTOBRE',
+    //     teamName1: 'Equipe 1',
+    //     teamLogo1: '../img/logo-ems.png',
+    //     score: '1 - 3',
+    //     teamName2: 'Equipe 2',
+    //     teamLogo2: '../img/logo-ems.png',
+    //     desc: 'MATCHS TEST - 12 OCOTBRE 2022 - 8H50 - ELBEUF'
+    // };
+    // console.log(match1);
+    // var match2 = {
+    //     mois: 'NOVEMBRE',
+    //     teamName1: 'Equipe 1 autre',
+    //     teamLogo1: '../img/logo-ems.png',
+    //     score: ' - ',
+    //     teamName2: 'Equipe 2 autre',
+    //     teamLogo2: '../img/logo-ems.png',
+    //     desc: 'TEST TEST - 2 NOVEMBRE 2023 - 18H20 - ROUEN'
+    // };
+
+    // var mois = document.querySelector('.carousel-info');
+
+    // console.log(mois.textContent);
+    // mois = match1.mois;
+    // console.log(mois);
+    // mois.innerHTML(match1.mois);
+
+    // document.querySelector('.btn-next').addEventListener('click', () => {
+    //     mois.innerHTML("<h3 class='carousel-info'>" + match1.mois + "</h3>");
+    //     console.log(match1.mois);
+    // });
 });
